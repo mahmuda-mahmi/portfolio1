88a340bda069df2894597234a0832d554d5e2f68
mahmudamahmi <mahmudapakter@gmail.com>
2024-03-28 01:19:09 +0600

    contact form added

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,8 +94,8 @@ $(function () {
                      {
                             breakpoint: 1024,
                             settings: {
-                                   slidesToShow: 3,
-                                   slidesToScroll: 3,
+                                   slidesToShow: 1,
+                                   slidesToScroll: 1,
                                    infinite: true,
                                    dots: true
                             }
@@ -103,8 +103,8 @@ $(function () {
                      {
                             breakpoint: 600,
                             settings: {
-                                   slidesToShow: 2,
-                                   slidesToScroll: 2
+                                   slidesToShow: 1,
+                                   slidesToScroll: 1
                             }
                      },
                      {
